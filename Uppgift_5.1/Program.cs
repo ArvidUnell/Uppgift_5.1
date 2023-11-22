17ef3c5b9d37a5559f0853f8fde5f5f0ece171ac
arun06001 <arun06001@E-PW0234KE>
2023-11-22 12:20:37 +0100

    Add project files.

---
+++ Uppgift_5.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namnArray = new string[5];
+
+            Console.WriteLine("Skriv in 5 namn på var sin rad");
+
+            for (int i = 0; i < namnArray.Length; i++)
+            {
+                namnArray[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nNamnen du skrev in var:");
+            foreach (string namn in namnArray)
+            {
+                Console.WriteLine(namn);
+            }
+
+            Console.WriteLine("\nTryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
